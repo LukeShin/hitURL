f26723a836268a4b1f3a0c49fae12f279a579040
Luke Shin <shingun@ideasam.net>
2021-07-11 20:40:53 +0900

    Part 2 Done!

--- main.go
+++ main.go
@@ -9,8 +9,11 @@ import (
 var errRequestFailed = errors.New("Request Failed!")
 
 func main() {
+	// 아래처럼 map 초기화를 해야한다. 아니면 nil이 된다!!
+	// var results = map[string]string{}
+	var results = make(map[string]string)
 	urls := []string{
-		"https://www.aribnb.com/",
+		"https://www.airbnb.com",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
 		"https://www.reddit.com/",
@@ -21,7 +24,15 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -29,6 +40,7 @@ func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
