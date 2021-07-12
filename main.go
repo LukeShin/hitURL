6149b4961da85171a0aa059997ab4893b759406f
Luke Shin <shingun@ideasam.net>
2021-07-12 17:07:21 +0900

    #3.5 One more Recap Done!

--- main.go
+++ main.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
-	people := [2]string{"Nico", "Luke"}
+	c := make(chan string)
+	people := [3]string{"Nico", "Luke", "John"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println("Waiting for ", i)
+		fmt.Println(<-c)
+	}
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 1)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 3)
+	c <- person + " is sexy."
 }
